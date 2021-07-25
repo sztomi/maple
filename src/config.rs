d86779f39feb2f60be9a929e64edb049ce9c4d4e
Tamás Szelei <szelei.t@gmail.com>
2021-07-25 18:04:35 +0200

    config: extract common logic from get&set

--- src/config.rs
+++ src/config.rs
@@ -1,16 +1,17 @@
 use std::fs::{self, File};
 use std::path::PathBuf;
 
-use anyhow::Result;
+use anyhow::{ensure, Result};
 use configparser;
 use directories::ProjectDirs;
 use log;
 use thiserror::Error;
 
+// TODO(sztomi): read this from the environment
+const CONFIG_FILE_NAME: &str = "mapleconfig.ini";
+
 #[derive(Error, Debug)]
 pub enum ConfigError {
-  #[error("Config file doesn't exist or empty.")]
-  NoConfig,
   #[error("Could not load config file: {ini_error:?}")]
   CouldNotLoadConfig { ini_error: String },
   #[error("No valid home location could be determined.")]
@@ -19,33 +20,7 @@ pub enum ConfigError {
   CouldNotWriteConfig { ini_error: String },
 }
 
-pub fn get(section: &str, key: &str) -> Result<Option<String>, ConfigError> {
-  if let Some(proj_dirs) = ProjectDirs::from("com", "Maple for Plex", "Maple") {
-    let cfg_file = PathBuf::from(format!("{}/maplecfg.ini", proj_dirs.config_dir().display()));
-    if !cfg_file.is_file() {
-      if let Ok(_) = File::create(&cfg_file) {
-        return Err(ConfigError::NoConfig);
-      }
-      else {
-        log::error!("Could not create config file at {}", cfg_file.display());
-        std::process::exit(-1);
-      }
-    }
-    let mut config = configparser::ini::Ini::new();
-    if let Err(e) = config.load(&cfg_file) {
-      return Err(ConfigError::CouldNotLoadConfig {
-        ini_error: e.to_string(),
-      });
-    }
-    match config.get(&section, &key) {
-      Some(val) => return Ok(Some(val.clone())),
-      None => return Ok(None),
-    }
-  }
-  Err(ConfigError::NoValidHome)
-}
-
-pub fn set(section: &str, key: &str, value: &str) -> Result<(), ConfigError> {
+pub fn ensure_config_file() -> Result<PathBuf, ConfigError> {
   if let Some(proj_dirs) = ProjectDirs::from("com", "Maple for Plex", "Maple") {
     let cfg_dir = PathBuf::from(proj_dirs.config_dir());
     if !cfg_dir.is_dir() {
@@ -58,7 +33,11 @@ pub fn set(section: &str, key: &str, value: &str) -> Result<(), ConfigError> {
         std::process::exit(-1);
       }
     }
-    let cfg_file = PathBuf::from(format!("{}/maplecfg.ini", proj_dirs.config_dir().display()));
+    let cfg_file = PathBuf::from(format!(
+      "{}/{}",
+      proj_dirs.config_dir().display(),
+      CONFIG_FILE_NAME
+    ));
     if !cfg_file.is_file() {
       if let Err(creation_err) = File::create(&cfg_file) {
         log::error!(
@@ -69,21 +48,40 @@ pub fn set(section: &str, key: &str, value: &str) -> Result<(), ConfigError> {
         std::process::exit(-1);
       }
     }
-    log::trace!(
-      "Writing {}.{} = {} to {}",
-      section,
-      key,
-      value,
-      cfg_file.display()
-    );
-    let mut config = configparser::ini::Ini::new();
-    config.set(&section, &key, Some((&value).to_string()));
-    if let Err(e) = config.write(cfg_file) {
-      return Err(ConfigError::CouldNotWriteConfig {
-        ini_error: e.to_string(),
-      });
-    }
-    return Ok(());
+    return Ok(cfg_file);
   }
   Err(ConfigError::NoValidHome)
+}
+
+pub fn get(section: &str, key: &str) -> Result<Option<String>, ConfigError> {
+  let cfg_file = ensure_config_file()?;
+  let mut config = configparser::ini::Ini::new();
+  if let Err(e) = config.load(&cfg_file) {
+    return Err(ConfigError::CouldNotLoadConfig {
+      ini_error: e.to_string(),
+    });
+  }
+  match config.get(&section, &key) {
+    Some(val) => Ok(Some(val.clone())),
+    None => Ok(None),
+  }
+}
+
+pub fn set(section: &str, key: &str, value: &str) -> Result<(), ConfigError> {
+  let cfg_file = ensure_config_file()?;
+  log::trace!(
+    "Writing {}.{} = {} to {}",
+    section,
+    key,
+    value,
+    cfg_file.display()
+  );
+  let mut config = configparser::ini::Ini::new();
+  config.set(&section, &key, Some((&value).to_string()));
+  if let Err(e) = config.write(cfg_file) {
+    return Err(ConfigError::CouldNotWriteConfig {
+      ini_error: e.to_string(),
+    });
+  }
+  Ok(())
 }
