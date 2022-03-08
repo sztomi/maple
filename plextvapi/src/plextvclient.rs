6d9c27ce9f90badb666b22556c9698176356d652
Tamás Szelei <szelei.t@gmail.com>
2022-03-08 20:04:35 +0100

    Fix example in docs

--- plextvapi/src/plextvclient.rs
+++ plextvapi/src/plextvclient.rs
@@ -95,24 +95,28 @@ impl PlexTvClient {
   /// One way to implement that with a browser is the following snippet:
   ///
   /// ```
-  /// let pin = client.create_pin().await?;
-  /// let auth_url = client.get_auth_url(&pin);
+  /// let pin = self.plextv.create_pin(true).await?;
+  /// let auth_url = self.plextv.get_auth_url(&pin);
   /// let mut tries = 0;
-  /// let token: Option<String> = None;
-  ///
+  /// const MAX_TRIES: u16 = 128;
+  /// let mut token: Option<String> = None;
   /// if webbrowser::open(&auth_url).is_ok() {
   ///   loop {
   ///     tries += 1;
-  ///     let pinf = client.try_pin(&pin).await?;
-  ///     tokio::time::delay_for(Duration::from_millis(1000)).await;
-  ///     if let Some(token) = pinf.auth_token {
-  ///       token = Some(token.clone()); // success
+  ///     let pinf = self.plextv.try_pin(&pin).await?;
+  ///     tokio::time::sleep(Duration::from_millis(1000)).await;
+  ///     if let Some(tk) = pinf.auth_token {
+  ///       token = Some(tk);
+  ///       self.window.upgrade_in_event_loop(|window| {
+  ///         window.set_selected_screen(1);
+  ///       });
   ///       break;
   ///     }
   ///     if tries > MAX_TRIES {
   ///       break; // failed to get a token after exhausting tries
   ///     }
   ///   }
+  /// }
   /// ```
   pub async fn create_pin(&mut self, strong: bool) -> Result<CreatePinResponse> {
     if self.token.is_some() {
